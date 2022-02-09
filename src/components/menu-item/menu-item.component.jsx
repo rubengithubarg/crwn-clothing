6d9d29b0726b86bc4380cf2397674ab35c896ba7
Ruben <emaranda94@gmail.com>
2022-02-09 19:12:23 -0300

    crwn-clothing v3

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const Menuitem = ({ title, imageUrl, size }) => (
-  <div style={{
+  <div className={`${size} menu-item`}>
+  <div className='background-image'
+   style={{
     backgroundImage: `url(${imageUrl})`
-  }}
-   className={`${size} menu-item`}>
-   {console.log(size, imageUrl)}
-  <div className='content'>
+  }} />
+   <div className='content'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <span className='subtitle'>SHOP NOW</span>
   </div>
