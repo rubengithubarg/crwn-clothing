42e8718ad80d185499ce8d01df9652750f340695
Ruben <emaranda94@gmail.com>
2022-02-09 18:24:38 -0300

    crwn-clothing v2

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,9 +5,11 @@ import './menu-item.styles.scss';
 const Menuitem = ({ title, imageUrl, size }) => (
   <div style={{
     backgroundImage: `url(${imageUrl})`
-  }} className={`${size} menu-item`}>
+  }}
+   className={`${size} menu-item`}>
+   {console.log(size, imageUrl)}
   <div className='content'>
-    <h1 className='title'>{ title }</h1>
+    <h1 className='title'>{title.toUpperCase()}</h1>
     <span className='subtitle'>SHOP NOW</span>
   </div>
 </div>
