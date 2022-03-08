1e1712648ea8aa2220d1439d698145a88ec88029
Ruben <emaranda94@gmail.com>
2022-03-08 17:39:48 -0300

    crwn-cloting v29

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -18,10 +19,9 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = ({ cart: { cartItems }}) => (
+const mapStateToProps = state => (
   {
-    itemCount: cartItems.reduce(
-      (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
+    itemCount: selectCartItemsCount(state)
   }
 )
 
